b97ae0fb77b2d74c49b90d13b201b2da1c3f7c45
tanyaleepr <tanyaleepr@gmail.com>
2021-12-30 10:51:53 -0600

    created arrays for possible character choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 // Assignment code here
   var generateBtn = document.querySelector("#generate");
 
+// Created Arrays of possible character choices in generating the password
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
+];
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
