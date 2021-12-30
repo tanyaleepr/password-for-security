7589b3a5a269272d37ee7b444db15b55c60c6e1d
tanyaleepr <tanyaleepr@gmail.com>
2021-12-30 10:49:21 -0600

    moved files to folder and created assignment code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-
+  var generateBtn = document.querySelector("#generate");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
