decd7375eef20f4475bd4d52124c70528628262a
tanyaleepr <tanyaleepr@gmail.com>
2021-12-30 11:14:40 -0600

    added comments and generated box with password input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // Assignment code here
   var generateBtn = document.querySelector("#generate");
 
-// Created Arrays of possible character choices in generating the password
+// Created Arrays of possible character choices in generating the password.
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ];
+
 
 // Created function for users to have options in choosing character options to ensure minimum requirements.
 function questions() {
@@ -34,8 +35,7 @@ function questions() {
   } while(!isValid);
   return responses;
 }
-
-// This function joins all the user responses and then creates the result - a strong password.
+// This function joins all the user responses and then creates the password.
 function generatePassword() {
   var passwordOptions = questions();
   var possibleCombo = [];
@@ -73,19 +73,12 @@ function generatePassword() {
   return finalPassword;
 }
 
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+// Write the results of "password" to the input in the box.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
+// Add event listener to generate button.
 generateBtn.addEventListener("click", writePassword);
