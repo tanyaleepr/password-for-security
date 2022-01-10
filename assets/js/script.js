8ef679245edf08cf3a54620c6aaa6e5e2d730004
tanyaleepr <tanyaleepr@gmail.com>
2022-01-09 21:35:11 -0600

    added comments for each section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-// Assignment code here
+// Assignment code here to start the password generator
   var generateBtn = document.querySelector("#generate");
 
-// Created Arrays of possible character choices in generating the password.
+// Created Arrays of possible character choices in generating the password. The more variables, the stronger the password. 
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -9,7 +9,7 @@ var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', 
 ];
 
 
-// Created function for users to have options in choosing character options to ensure minimum requirements.
+// Created function for clients to choose their character options and ensure minimum requirements.
 function questions() {
   var isValid = false;
   do {
@@ -35,7 +35,7 @@ function questions() {
   } while(!isValid);
   return responses;
 }
-// This function joins all the user responses and then creates the password.
+// This function combines the user responses using the variables above to create the password.
 function generatePassword() {
   var passwordOptions = questions();
   var possibleCombo = [];
@@ -73,7 +73,7 @@ function generatePassword() {
   return finalPassword;
 }
 
-// Write the results of "password" to the input in the box.
+// Write the results of the secured "password" as the input in the box.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
